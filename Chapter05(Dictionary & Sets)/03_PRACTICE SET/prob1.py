912d574c80d8e87c66b9eccdfde9b1350feb6d1f
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-05-01 04:11:51 +0530

    Add Hindi-English dictionary program with user lookup functionality

---
+++ Chapter05(Dictionary & Sets)/03_PRACTICE SET/prob1.py
@@ -0,0 +1,10 @@
+# Write a program to create a dictionary of Hindi words with values as their English translation. Provide user with an option to look it up!
+
+words = {
+    
+    "नमस्ते": "Hello",
+    "kursi": "Chair",
+    "billi": "Cat",
+} 
+word = input("Enter a Hindi word meaning of: ")
+print(words[word])
