33bb4dded441c1706ed29bff918f171cddabcf3f
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-05-01 03:58:46 +0530

    Update list.py to include a diverse list of friends and demonstrate element access and modification

--- Chapter04(List And Tuples)/01_Lists/list.py
+++ Chapter04(List And Tuples)/01_Lists/list.py
@@ -1,2 +1,5 @@
 # Python lists are containers to store a set of values of any data type.
-friends = ["Apple", ""]
+friends = ["Apple", "Orange", 5, 234.23, False, "Adil", "Raza"] # List of different data types
+print(friends[0])
+friends[0] = "Grapes" # Updating the value of the first element
+print(friends[0]) 
