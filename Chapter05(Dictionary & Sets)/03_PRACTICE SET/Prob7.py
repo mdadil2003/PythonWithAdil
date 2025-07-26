2e77e1c1b6b2fd17f803185c3927528b2fd5fff3
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-07-27 05:26:50 +0530

    mutable types in sets

---
+++ Chapter05(Dictionary & Sets)/03_PRACTICE SET/Prob7.py
@@ -0,0 +1,6 @@
+# Can you change the values inside a list which is contained in set S?
+s = {8, 7, 12, "Harry", [1,2]}
+ 
+# What will be the output of the above code?
+# The code will raise a TypeError because lists are mutable and cannot be added to a set.
+# The output will be an error message indicating that lists are unhashable types.
