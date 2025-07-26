36ff5544dd5ba000f62b69e5fa3e419281640d48
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-07-27 04:48:55 +0530

    Add program to collect favorite languages from friends and store in a dictionary

---
+++ Chapter05(Dictionary & Sets)/03_PRACTICE SET/Prob6.py
@@ -0,0 +1,25 @@
+# Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique
+
+d = {}
+
+name = input("Enter freiends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter freiends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter freiends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter freiends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+name = input("Enter freiends name: ")
+lang = input("Enter language name: ")
+d.update({name: lang})
+
+print(d)
