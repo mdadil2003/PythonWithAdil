0c61a679f4f6072a45d0061f342ff9952805607c
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-07-28 02:13:57 +0530

    Add age consent check program

---
+++ Chapter06(Conditional Expression)/Conditionals.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your age: "))
+
+if (a>=18):
+    print("You are above the age of consent")
+    print("Good for you!")
+    
+else:
+    print("You are not above the age of consent")
+    
