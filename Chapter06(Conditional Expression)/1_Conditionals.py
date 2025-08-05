a9188ec3b8711a723e1ff67816dfb24f9cc20fda
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-08-06 04:30:44 +0530

    conditional statement checks

--- Chapter06(Conditional Expression)/1_Conditionals.py
+++ Chapter06(Conditional Expression)/1_Conditionals.py
@@ -1,9 +1,12 @@
 a = int(input("Enter your age: "))
 
+#if else statement 
+
 if (a>=18):
     print("You are above the age of consent")
     print("Good for you!")
     
 else:
     print("You are not above the age of consent")
-    print("You are not old enough to consent")
+    
+print("End of Program")
---
+++ Chapter06(Conditional Expression)/2_if_elif_else_ladder.py
@@ -0,0 +1,19 @@
+a = int(input("Enter your age: "))
+
+# if elif else ladder to check age and print appropriate messages
+
+if (a>=18):
+    print("You are above the age of consent")
+    print("Good for you!")
+    
+elif(a<0):
+    print("You are entering an invalid age")
+    
+elif(a==0):
+    print("You are entering 0 which is not a valid age")
+    
+else:
+    print("You are not above the age of consent")
+    
+
+print("End of Program")
