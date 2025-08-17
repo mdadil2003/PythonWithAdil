b56b41a431a0f1530ecc957660d4961827b13602
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-08-18 02:05:25 +0530

    Learning & Practice

--- Chapter08(Fuctions And Recursions)/02_Recursion/factorial.py
+++ Chapter08(Fuctions And Recursions)/02_Recursion/factorial.py
@@ -8,6 +8,17 @@ factorial(5) = 1 x 2 x 3 x 4 x 5
 
 factorial(n) = n x n-1 x ........3 x 2 x 1
 
-factorial(n) = 
+factorial(n) = n * factorial(n-1)
 
 '''
+def factorial(n):
+    if (n ==1 or n == 0):
+        
+        return 1
+    else:
+        return n * factorial(n-1)
+
+
+n = int(input("Enter a number: "))
+print(f"The factorial of this number is : {factorial(n)}")
+
---
+++ Chapter08(Fuctions And Recursions)/03_Practice Set/01_Prob.py
@@ -0,0 +1,21 @@
+# 1. Write a program using functions to find greatest of three numbers.
+
+a = 1 
+b = 2
+c = 3
+
+def greatest(a, b, c):
+    if(a>b and a>c):
+        return a
+    elif(b>a and b>c):
+        return b
+    elif(c>b and c>a):
+        return c
+    
+    a = 1
+    b = 2
+    c = 3
+    
+    print(f"The greatest number is: {greatest(a, b, c)}")
+    
+    
