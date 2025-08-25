bc418fa02744743d75b8f4fe90f445b9ed99a3c1
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-08-26 02:34:40 +0530

    Learn & Pratices

--- Chapter08(Fuctions And Recursions)/03_Practice Set/02_Prob.py
+++ Chapter08(Fuctions And Recursions)/03_Practice Set/02_Prob.py
@@ -1,5 +1,8 @@
 # 2. Write a python program using function to convert Celsius to Fahrenheit.
 
+'''
+# convert Celsius to Fahrenheit.
+
 def celsius_to_fahrenheit(celsius):
     fahrenheit = (celsius * 9/5) + 32
     return fahrenheit
@@ -7,5 +10,17 @@ def celsius_to_fahrenheit(celsius):
 celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = celsius_to_fahrenheit(celsius)
 print(f"{celsius} Celsius is equal to {fahrenheit} Fahrenheit.")
- 
- 
+
+'''
+
+# convert Fahrenheit to Celsius.
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = fahrenheit_to_celsius(fahrenheit)
+print(f"{fahrenheit} Fahrenheit is equal to {celsius} Celsius.")
+
+
