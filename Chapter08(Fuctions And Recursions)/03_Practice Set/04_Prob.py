a12afd7d173230d18ab1e951ef4661a183940ae9
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-09-10 14:41:08 +0530

    Practices

--- Chapter08(Fuctions And Recursions)/03_Practice Set/04_Prob.py
+++ Chapter08(Fuctions And Recursions)/03_Practice Set/04_Prob.py
@@ -7,4 +7,5 @@ def sum_of_natural_numbers(n):
         return n + sum_of_natural_numbers(n - 1)
 
 n = int(input("Enter a positive integer: "))
+
 print(f"The sum of the first {n} natural numbers is: {sum_of_natural_numbers(n)}")
