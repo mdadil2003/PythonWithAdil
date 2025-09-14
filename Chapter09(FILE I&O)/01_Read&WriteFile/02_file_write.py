38b10b19a20a49982811462ced62b194dbb05213
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-09-14 14:51:10 +0530

    Learn & Practices

--- Chapter09(FILE I&O)/01_Read&WriteFile/02_file_write.py
+++ Chapter09(FILE I&O)/01_Read&WriteFile/02_file_write.py
@@ -1,4 +1,4 @@
-st = "hey Adil you are an amazing coder......keep it up"
+st = "(hey Adil you are an amazing coder......keep it up for future data analytics :)"
 
 f = open("write.txt", "w") # opens the file in write mode
 
