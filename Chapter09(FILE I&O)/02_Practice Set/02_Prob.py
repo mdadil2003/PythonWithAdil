3d6691d9e70fc36748459a44243f6c5e65c66f91
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-09-25 02:42:36 +0530

    practice

--- Chapter09(FILE I&O)/02_Practice Set/02_Prob.py
+++ Chapter09(FILE I&O)/02_Practice Set/02_Prob.py
@@ -1,2 +1,16 @@
 # The game() function in a program lets a user play a game and returns the score as an integer. You need to read a file ‘Hi-score.txt’ which iseither blank or contains the previous Hi-score. You need to write a program to update the Hiscore whenever the game() function breaks the Hi-score.
 
+import random
+
+def game():
+    print("you are playing the game..")
+    score = random.randint(1, 62)
+    print(f"your score: {score}")
+    if(score>hiscore or hiscore==""):
+        
+    return score
+game()
+    
+    print ("congratulations! you have just broken the hiscore")
+    print("your hiscore is now:", score)
+    
