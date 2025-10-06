8a603d293eff70e3a3465cf1835ead947038bb7b
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-10-07 03:26:50 +0530

    Employee class with attributes and instance creation

---
+++ Chapter10(Oops)/class.py
@@ -0,0 +1,11 @@
+class Employee:  # defining a class
+    
+    language = "Python" # this is a class attribute
+    company = "ZOHO"
+    salary = 100000
+    location = "Delhi"
+    role = "SDE"
+    
+adil = Employee() 
+adil.name = "Adil" # this is an instance attribute
+print(adil.name, adil.company, adil.salary, adil.location, adil.role, adil.language) 
