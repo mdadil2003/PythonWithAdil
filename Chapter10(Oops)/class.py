02e9981f25078132bcaa71f299fd34b403e62242
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-10-08 00:09:43 +0530

    Learn & Practice

--- Chapter10(Oops)/class.py
+++ Chapter10(Oops)/class.py
@@ -8,4 +8,4 @@ class Employee:  # defining a class
     
 adil = Employee() 
 adil.name = "Adil" # this is an instance attribute
-print(adil.name, adil.company, adil.salary, adil.location, adil.role, adil.language) 
+print(adil.name, adil.company, adil.salary, adil.location, adil.role, adil.language)
