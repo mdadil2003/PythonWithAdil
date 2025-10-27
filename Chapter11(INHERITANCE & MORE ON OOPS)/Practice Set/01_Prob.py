19eb9b2c8ba7fb32a454f4d282ce99a2a507a945
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-10-28 00:10:04 +0530

    Add Vector2D and Vector3D classes with display methods

---
+++ Chapter11(INHERITANCE & MORE ON OOPS)/Practice Set/01_Prob.py
@@ -0,0 +1,22 @@
+# Create a class (2-D vector) and use it to create another class representing a 3-D vector.
+
+class Vector2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def show(self):
+        print(f"Vector2D: ({self.x}, {self.y})")    
+
+class Vector3D(Vector2D):  # Inheriting from Vector2D
+    def __init__(self, x, y, z):
+        super().__init__(x, y)
+        self.z = z
+
+    def show(self):
+        print(f"Vector3D: ({self.x}, {self.y}, {self.z})")
+
+a = Vector2D(1, 2) # Creating an object of Vector2D
+a.show()
+b = Vector3D(4, 5, 6) # Creating an object of Vector3D
+b.show()
