8566526a7a12ddd91f016921b1e1264414da4174
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-11-12 00:50:13 +0530

    Auto Reply AI Chatbot

---
+++ Projects/Auto Reply AI Chatbot/01_get_cursor.py
@@ -0,0 +1,10 @@
+# Auto Reply AI Chatbot
+
+import pyautogui
+
+
+while True:
+    a = pyautogui.position()
+    print(a)
+    
+    
---
+++ Projects/Auto Reply AI Chatbot/03_bot.py
@@ -0,0 +1,24 @@
+import pyautogui
+import time
+import pyperclip
+
+# Wait a few seconds before starting (to give you time to open window)
+time.sleep(3)
+
+# Step 1: Click on the icon
+pyautogui.click(1639, 1412)
+time.sleep(1)  # small delay
+
+# Step 2: Drag to select text
+pyautogui.moveTo(626, 238)
+pyautogui.dragTo(1199, 944, duration=1, button='left')
+
+# Step 3: Copy selected text
+pyautogui.hotkey('ctrl', 'c')
+time.sleep(0.5)
+
+# Step 4: Get copied text from clipboard
+copied_text = pyperclip.paste()
+
+print("✅ Copied Text:\n")
+print(copied_text)
