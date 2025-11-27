b7e8d7eabdf5e895fe68b08e112d26ab67ae6f98
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-11-28 04:48:57 +0530

    `Updated musicLibrary.py with a single song title change from "Sukun" to "Sukune dil".`

--- Projects/Jarvis Voice Assistant/musicLibrary.py
+++ Projects/Jarvis Voice Assistant/musicLibrary.py
@@ -1,5 +1,5 @@
 music = {
-    "Sukun": "https://www.youtube.com/watch?v=XlyKuYIr7Gc&list=RDXlyKuYIr7Gc&t=976s",
+    "Sukune dil": "https://www.youtube.com/watch?v=XlyKuYIr7Gc&list=RDXlyKuYIr7Gc&t=976s",
     "Andaaz e Karam": "https://www.youtube.com/watch?v=__bHEfJ29j0&list=RDXlyKuYIr7Gc&index=2",
     "Ghazal": "https://www.youtube.com/watch?v=LnVW-lYaRIs&list=RDXlyKuYIr7Gc&index=4&pp=8AUB0gcJCQMKAYcqIYzv"    
 }
