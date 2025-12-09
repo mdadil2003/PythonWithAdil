d05e3ec922cf5e1a109445dca41be0f8442e4742
Md Adil Raza <adilsiddiqei30@gmail.com>
2025-12-10 00:16:19 +0530

    alternative model option in OpenAI function

--- Projects/Auto Reply AI Chatbot/02_openAi.py
+++ Projects/Auto Reply AI Chatbot/02_openAi.py
@@ -9,7 +9,7 @@ command = ''''
 Mujhe ek aisa python function likh ke do jo ki ek list of numbers ko input le aur usme se sirf prime numbers ko return kare.
 '''
 completion = client.chat.completions.create(
-   model= "gpt-3.5-turbo", # ya "gpt-4"  
+   model= "gpt-3.5-turbo",   
    messages=[
        {"role": "system", "content": "You are a person named Adil who speaks hindi as well as english. He is from India and is coder. You analyze chat history and respond like Adil"},
        {"role": "user", "content": command} # user prompt
